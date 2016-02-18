51e39241214d0578da6e956a7a31b7884d267e54
gersteinj <gersteinj@gmail.com>
2016-02-18 12:14:43 -0500

    file name is word displayed

--- pythonVersion/TextToImages.py
+++ pythonVersion/TextToImages.py
@@ -4,29 +4,35 @@ import svgwrite
 imageW = 500
 imageH = 300
 textSize = 75
+count = 1
 
-# TODO: Load strings from a list
+# Load strings from a list
+wordList = open('wordlist.txt')
+lines = wordList.readlines()
 
-# TODO: Grab the next string
+# Grab the next string
+for line in lines:
+	print(line)
 
-# TODO: Set size variables based on length of word
+	# TODO: Set size variables based on length of word
 
-# create an SVG file
-dwg = svgwrite.Drawing('testfile.svg', (imageW, imageH), profile = 'tiny')
+	# create an SVG file
+	dwg = svgwrite.Drawing(str(line.rstrip())+'.svg', (imageW, imageH), profile = 'tiny')
 
-# Draw background
-dwg.add(dwg.rect(insert = (0,0), size = ('100%', '100%'), fill = 'purple'))
+	# Draw background
+	dwg.add(dwg.rect(insert = (0,0), size = ('100%', '100%'), fill = 'purple'))
 
-# TODO: Draw text
-dwg.add(dwg.text("Hello world",
-	insert = (imageW/2, imageH/2 + textSize*.25	),
-	text_anchor = 'middle',
-	fill = 'white',
-	font_size = str(textSize),
-	font_family = 'Arial'
-	))
+	# TODO: Draw text
+	dwg.add(dwg.text(line,
+		insert = (imageW/2, imageH/2 + textSize*.25	),
+		text_anchor = 'middle',
+		fill = 'white',
+		font_size = str(textSize),
+		font_family = 'Arial'
+		))
 
-# Save file
-dwg.save()
+	# Save file
+	dwg.save()
+	count += 1
 
 # TODO: Convert SVG to image file
