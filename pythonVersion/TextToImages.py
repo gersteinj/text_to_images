d2218ef5fc47cfe85ced00fa2f32fadb5c29cd57
gersteinj <gersteinj@gmail.com>
2016-02-18 10:26:05 -0500

    outline created

---
+++ pythonVersion/TextToImages.py
@@ -0,0 +1,18 @@
+import svgwrite
+
+# TODO: Load strings from a list
+
+# TODO: Grab the next string
+
+# TODO: Set size variables based on length of word
+
+# create an SVG file
+dwg = svgwrite.Drawing('testfile.svg', (500,300), profile = 'tiny')
+
+# TODO: Draw background
+
+# TODO: Draw text
+
+# TODO: Save file
+
+# TODO: Convert SVG to image file
