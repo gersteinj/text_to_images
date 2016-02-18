d50b72d9748251c60298218b56d5bdaffd1f1ce9
gersteinj <gersteinj@gmail.com>
2016-02-18 10:38:34 -0500

    centering text

--- pythonVersion/TextToImages.py
+++ pythonVersion/TextToImages.py
@@ -1,5 +1,9 @@
 import svgwrite
 
+# Create relevant variables
+imageW = 500
+imageH = 300
+
 # TODO: Load strings from a list
 
 # TODO: Grab the next string
@@ -7,13 +11,13 @@ import svgwrite
 # TODO: Set size variables based on length of word
 
 # create an SVG file
-dwg = svgwrite.Drawing('testfile.svg', (500,300), profile = 'tiny')
+dwg = svgwrite.Drawing('testfile.svg', (imageW, imageH), profile = 'tiny')
 
 # Draw background
 dwg.add(dwg.rect(insert = (0,0), size = ('100%', '100%'), fill = 'purple'))
 
 # TODO: Draw text
-dwg.add(dwg.text("I AM TEXT", insert = (0,100), fill = 'white', stroke = 'black'))
+dwg.add(dwg.text("I AM TEXT", insert = (imageW/2, imageH/2), text_anchor = 'middle', fill = 'white', stroke = 'black'))
 
 # Save file
 dwg.save()
