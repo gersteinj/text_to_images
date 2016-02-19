cd5802840f7c97f50708c8d754e3d2a54ec17824
gersteinj <gersteinj@gmail.com>
2016-02-19 12:46:13 -0500

    fixed problems with punctuation by changing file name style

--- pythonVersion/TextToImages.py
+++ pythonVersion/TextToImages.py
@@ -1,8 +1,7 @@
 import svgwrite
 
 # Create relevant variables
-imageW = 300
-imageH = 300
+prefix = 'test'
 textSize = 75
 count = 1
 letterSize = textSize*.75
@@ -20,18 +19,23 @@ for line in lines:
 	# TODO: Can I figure out a way to do it based on the actual width of the word?
 	print(len(line))
 	imageW = len(line) * letterSize
+	imageH = letterSize * 2
 
 	# create an SVG file
-	dwg = svgwrite.Drawing(str(line)+'.svg', (imageW, imageH), profile = 'tiny')
+	# dwg = svgwrite.Drawing(str(line)+'.svg', (imageW, imageH), profile = 'tiny')
+	dwg = svgwrite.Drawing(str(prefix)+str(count)+'.svg', (imageW, imageH), profile = 'tiny')
 
 	# Draw background
-	dwg.add(dwg.rect(insert = (0,0), size = ('100%', '100%'), fill = 'purple'))
+	dwg.add(dwg.rect(insert = (0,0),
+		size = ('100%', '100%'),
+		stroke = 'black',
+		fill = 'white'))
 
 	# Draw text
 	dwg.add(dwg.text(line,
 		insert = (imageW/2, imageH/2 + textSize*.25	),
 		text_anchor = 'middle',
-		fill = 'white',
+		fill = 'black',
 		font_size = str(textSize),
 		font_family = 'Arial'
 		))
