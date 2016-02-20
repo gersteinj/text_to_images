0f6e829c27e2dfecda84532563ce514b5cef423a
gersteinj <gersteinj@gmail.com>
2016-02-20 18:57:14 -0500

    first version using PIL

--- pythonVersion/TextToImages.py
+++ pythonVersion/TextToImages.py
@@ -1,10 +1,15 @@
 import svgwrite
+import PIL
+from PIL import Image
+from PIL import ImageDraw
+print('svg')
+print('pillow')
 
 # Create relevant variables
 prefix = 'test'
 textSize = 75
 count = 1
-letterSize = textSize*.75
+letterSize = textSize * .75
 
 # Load strings from a list
 wordList = open('wordlist.txt')
@@ -13,35 +18,24 @@ lines = words.splitlines()
 
 # Grab the next string
 for line in lines:
-	print(line)
+    print(line)
 
-	# Set size variables based on length of word
-	# TODO: Can I figure out a way to do it based on the actual width of the word?
-	print(len(line))
-	imageW = len(line) * letterSize
-	imageH = letterSize * 2
+    # Set size variables based on length of word
+    # TODO: Can I figure out a way to do it based on the actual width of the
+    # word?
+    print(len(line))
+    imageW = len(line) * letterSize
+    imageH = letterSize * 2
 
-	# create an SVG file
-	# dwg = svgwrite.Drawing(str(line)+'.svg', (imageW, imageH), profile = 'tiny')
-	dwg = svgwrite.Drawing(str(prefix)+str(count)+'.svg', (imageW, imageH), profile = 'tiny')
+    # create a new image
+    img = Image.new("RGB", (int(imageW), int(imageH)), (200, 50, 255))
 
-	# Draw background
-	dwg.add(dwg.rect(insert = (0,0),
-		size = ('100%', '100%'),
-		stroke = 'black',
-		fill = 'white'))
+    # TODO: set font size
 
-	# Draw text
-	dwg.add(dwg.text(line,
-		insert = (imageW/2, imageH/2 + textSize*.25	),
-		text_anchor = 'middle',
-		fill = 'purple',
-		font_size = str(textSize),
-		font_family = 'Arial'
-		))
+    # Draw text
+    draw = ImageDraw.Draw(img)
+    draw.text((0, imageH/2), line, fill='white')
 
-	# Save file
-	dwg.save()
-	count += 1
-	
-	# TODO: Convert SVG to image file
+    # Save file
+    img.save(str(count)+'.png')
+    count += 1
