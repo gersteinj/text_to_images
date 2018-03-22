ac239dd97d84236e2e7903d4c0db30798f73b734
gersteinj <gersteinj@gmail.com>
2018-03-21 20:07:48 -0400

    created image-gen function

--- flaskapp/ttiapp.py
+++ flaskapp/ttiapp.py
@@ -11,10 +11,8 @@ def hello_world():
     # return render_template('index.html')
     return "Welcome to Text to Images"
 
-@app.route('/tti/<word>')
-@app.route('/tti/<word>/<sz>')
-def tti(word, sz=30):
-    # Set font and other variables
+def make_magnet(word, sz):
+# Set font and other variables
     fnt = ImageFont.truetype('LinBiolinum_Rah.ttf', int(sz))
     word = '  ' + word + '  '
     bg_color = 'white'
@@ -34,7 +32,34 @@ def tti(word, sz=30):
     img = img.resize((w, int(h*1.2)))
     draw = ImageDraw.Draw(img)
     draw.text((0,0), word, font=fnt, fill=text_color)
+    return img
+
+@app.route('/tti/<word>')
+@app.route('/tti/<word>/<sz>')
+def tti(word, sz=30):
+    # # Set font and other variables
+    # fnt = ImageFont.truetype('LinBiolinum_Rah.ttf', int(sz))
+    # word = '  ' + word + '  '
+    # bg_color = 'white'
+    # text_color = 'black'
+
+    # # create a new image
+    # img = Image.new("RGB", (10, 10), bg_color)
+
+    # # draw text
+    # # create a draw object
+    # draw = ImageDraw.Draw(img)
+
+    # # use textsize to find size of text
+    # w, h = draw.textsize(word, font=fnt)
     
+    # # resize image to fit text
+    # img = img.resize((w, int(h*1.2)))
+    # draw = ImageDraw.Draw(img)
+    # draw.text((0,0), word, font=fnt, fill=text_color)
+    
+    img = make_magnet(word, sz)
+
     # save and return file
     # TODO: figure out how to create file in memory
     img.save('test.png')
